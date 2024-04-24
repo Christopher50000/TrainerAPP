69f80993c2743e6a0dc124989d314239d66231b5
Christopher Tupa <christoper.tupa61@gmail.com>
2024-04-23 22:08:46 -0500

    Added User Object, made an endpoint to return a user object,  and created two other endpoints which helped me  learn the diff when to use  @PathVariable and @RequestParam

--- trainer-application/src/main/java/com/example/trainerApplication/controllers/TrainerRestController.java
+++ trainer-application/src/main/java/com/example/trainerApplication/controllers/TrainerRestController.java
@@ -1,11 +1,11 @@
 package com.example.trainerApplication.controllers;
 
 
+import com.example.trainerApplication.models.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RestController
 public class TrainerRestController {
@@ -16,9 +16,34 @@ public class TrainerRestController {
         return "Tom";
     }
 
+
+
+    //Use @PathVariable for mandatory data in the URI path.
+    //Use @RequestParam for optional data in the query string or form data.
+    // to retrieve a single resource (like a single user), @PathVariable is appropriate.
+    // But if you're searching or filtering a group of resources (like searching for users by name),
+    // @RequestParam is the way to go.
     @GetMapping("trainer/{id}")
     public long getTrainerId(@PathVariable long id)
     {
         return id;
     }
+
+    @GetMapping("trainerReq")
+    public String getTrainerIdQueryParam(@RequestParam long id)
+    {   // for some reason does not want to return values less than 100
+        System.out.println(id);
+        return String.valueOf(id);
+    }
+
+    @GetMapping("user")
+    public User getRandomUser()
+    {
+        User random = new User(1,"Chris");
+
+        return random;
+    }
+
+
+
 }
---
+++ trainer-application/src/main/java/com/example/trainerApplication/models/User.java
@@ -0,0 +1,18 @@
+package com.example.trainerApplication.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    long id;
+    String name;
+
+
+
+
+}
