144b362c5f2cf692aa1abfa5980eaf6668e034ef
Christopher Tupa <christoper.tupa61@gmail.com>
2024-05-12 21:59:16 -0500

    Added ability to create preset values for TrainerType Entity

--- trainer-application/init.sql
+++
@@ -1,8 +0,0 @@
--- Create the trainer_type table
-CREATE TABLE IF NOT EXISTS trainer_type (
-    id INTEGER PRIMARY KEY,
-    type_name VARCHAR(255) NOT NULL
-);
--- Insert mappings into trainer_type table
-INSERT INTO trainer_type (id, type_name) VALUES (1, 'Personal Trainer');
-INSERT INTO trainer_type (id, type_name) VALUES (2, 'Strength and Conditioning Coach');
---
+++ trainer-application/src/main/java/com/example/trainerApplication/models/entities/TrainerType.java
@@ -0,0 +1,21 @@
+package com.example.trainerApplication.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="trainer_type")
+@NoArgsConstructor
+@Data
+public class TrainerType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "type_name")
+    private String typeName;
+
+    @Column(name = "description")
+    private String description;
+}
