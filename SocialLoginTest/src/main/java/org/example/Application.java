91579c08a79a71d340b12cc6311c3ce69094686f
Christopher Tupa <christoper.tupa61@gmail.com>
2024-08-25 14:42:52 -0500

    Remove all application.properties files from repo

---
+++ SocialLoginTest/src/main/java/org/example/Application.java
@@ -0,0 +1,15 @@
+package com.example.trainerApplication.SocialLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.example.trainerApplication.TrainerApplication.class, args);
+    }
+
+}
